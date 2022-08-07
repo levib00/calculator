e43cf35d3fcc4090021c1db1c251fe281216a100
levib00 <levib8@hotmail.com>
2022-08-07 18:11:15 -0400

    allow for other operators than addition

--- scripts/calc.js
+++ scripts/calc.js
@@ -9,6 +9,10 @@ const number = document.querySelectorAll(".number-button");
 const screen = document.querySelector("#screen");
 number.forEach((numbers) => {
     numbers.addEventListener('click', () => {
+        if (result != null) {
+            screen.innerHTML = '';
+            result = null
+        }
         if (a === null && inputSwitch === false) {
             a = numbers.innerHTML;
             screen.innerHTML = a;
@@ -24,16 +28,73 @@ number.forEach((numbers) => {
         }     
     });
 });
-let operator = null
+let operator = null //TODO in current build you dont need to press an operator after calculating. i would like to change that.
 const add = document.querySelector("#plus-btn")
 add.addEventListener('click', addButton)
-function addButton(a, b) {
+function addButton() {
+    if (operator != null) {
+        calculate()
+    }
     inputSwitch = true
     operator = "+"
 }
-
+const subtract = document.querySelector("#minus-btn")
+subtract.addEventListener('click', subtractButton)
+function subtractButton() {
+    if (operator != null) {
+        calculate()
+    }
+    inputSwitch = true
+    operator = "-"
+}
+const multiply = document.querySelector("#multiply-btn")
+multiply.addEventListener('click', multiplyButton)
+function multiplyButton() {
+    if (operator != null) {
+        calculate()
+    }
+    inputSwitch = true
+    operator = "*"
+}
+const divide = document.querySelector("#division-btn")
+divide.addEventListener('click', divideButton)
+function divideButton() {
+    if (operator != null) {
+        calculate()
+    }
+    inputSwitch = true
+    operator = "/"
+}
+let result = null
 const equals = document.querySelector("#equals-btn")
 equals.addEventListener('click', calculate)
 function calculate() {
-    screen.innerHTML = parseFloat(a) + parseFloat(b)
+    switch (operator) {
+        case '+':
+            result = parseFloat(a) + parseFloat(b);
+            break;
+        case '-':
+            result = parseFloat(a) - parseFloat(b);
+            break;
+        case '/' : 
+            result = parseFloat(a) / parseFloat(b);
+            break;
+        case '*':
+            result = parseFloat(a) * parseFloat(b);
+            break
+    }
+    screen.innerHTML = result
+    a = result;
+    b = null
+    operator = null
+}
+
+const clear = document.querySelector("#clear-btn")
+clear.addEventListener('click', clearButton)
+function clearButton() {
+    screen.innerHTML = '';
+    a = null;
+    b = null;
+    operator = null;
+    inputSwitch = false
 }
